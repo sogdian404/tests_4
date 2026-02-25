bc3af8e64600421fc128789c1a05b6e2469473d3
sogdian404 <sogdian404@gmail.com>
2026-02-25 14:36:58 +0500

    Intitial commit

---
+++ CI/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public override double Area()
+        {
+            if (Height < 0 || Width < 0)
+            {
+                throw new Exception("Такой прямоугольник не существует");
+            }
+            return Width*Height;
+        }
+        public override double Perimeter()
+        {
+            if (Height < 0 || Width < 0)
+            {
+                throw new Exception("Такой прямоугольник не существует");
+            }
+            return 2*(Height+Width);
+        }
+        public override string ToString()
+        {
+            return $"Прямоугольник с сторонами: {Width}, {Height}";
+        }
+    }
+}
---
+++ CI/Triangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI
+{
+    public class Triangle : Shape
+    {
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public double SideA {  get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public override double Area()
+        {
+            if (SideA < 0 || SideB <0 || SideC<0 || SideA+SideB<SideC || SideC + SideB < SideA || SideA + SideC < SideB)
+            {
+                throw new Exception("Такой треугольник не существует");
+            }
+            double p = (SideA + SideB + SideC)/2;
+            return Math.Sqrt(p*(p-SideA)*(p-SideB)*(p-SideC));
+        }
+        public override double Perimeter()
+        {
+            if (SideA < 0 || SideB < 0 || SideC < 0)
+            {
+                throw new Exception("Такой треугольник не существует");
+            }
+            return SideA+SideB+SideC;
+        }
+        public override string ToString()
+        {
+            return $"Треугольник с сторонами {SideA}, {SideB}, {SideC}";
+        }
+    }
+}
